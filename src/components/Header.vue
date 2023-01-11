27ff42999a378df1ba5436dde1a62923f473c222
Bekmuratov-MBF <bekmuratovdilmurat18@gmail.com>
2023-01-11 10:57:38 +0300

    second commit

---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-container
+        max-width="1400"
+    >
+        <v-app-bar>
+            <v-app-bar-title><router-link to="/">Vue3</router-link></v-app-bar-title>
+            <v-spacer/>
+
+            <v-responsive
+                class="mx-auto"
+                max-width="344"
+            >
+                <v-text-field
+                label="Search"
+                hide-details="auto"
+                ></v-text-field>
+            </v-responsive>
+            <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-spacer/>
+            <v-btn>
+                <router-link to="/">Главная</router-link>
+            </v-btn>
+            <v-btn>
+                <router-link to="/services">Услуги</router-link>
+            </v-btn>
+            <v-btn class="login__btn" >
+                <router-link to="/login">Войти</router-link>
+            </v-btn>
+            <v-btn>
+                <router-link to="/user">
+                    Бекмуратов
+                </router-link>
+            </v-btn>
+        </v-app-bar>
+    </v-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.login__btn{
+    background-color: rgb(79, 151, 81);
+    color: white;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import MainPage from "@/pages/MainPage"; 
+import LoginPage from "@/pages/LoginPage";
+import ProfilePage from "@/pages/ProfilePage.vue";
 
 const routes = [
   {
-   
+    path: '/',
+    component: MainPage
   },
   {
-  
+    path: '/login',
+    component: LoginPage
+  },
+  {
+    path: '/user',
+    component: ProfilePage
   }
 ]
 
